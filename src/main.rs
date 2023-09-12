8f732c98a216752af470766d1876c47fe917ae98
Adeohluwa <adejumoadeoluwa@gmail.com>
2023-09-12 18:27:34 +0100

    Infinte Loop

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,25 @@ fn main() {
     // secret
     let secret_number = rand::thread_rng().gen_range(1..=100);
     let mut guess = String::new();
-    println!("Guess the number!");
+   
+    loop {
+        println!("Please input your Guess!");
+        
+        // accept input and bind to guess var, also handle error
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");       
     
-    // accept input and bind to guess var, also handle error
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to readline");       
-
-    // sanitize guess before comparison
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");    
-    println!("You guessed {guess}");
-    println!("The secret number is {secret_number}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small!"),
-        Ordering::Greater => println!("too big!"),
-        Ordering::Equal => println!("you win!"),
+        // sanitize guess before comparison
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");    
+        println!("You guessed {guess}");
+        println!("The secret number is {secret_number}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => println!("you win!"),
+        }
     }
 
 }
