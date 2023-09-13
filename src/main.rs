578edef2288e5b2a1fb5e24ccf0acac325a00a75
Adeohluwa <adejumoadeoluwa@gmail.com>
2023-09-13 15:32:50 +0100

    fn definition, Implicit return

--- src/main.rs
+++ src/main.rs
@@ -21,4 +21,23 @@ fn main() {
         println!("The value of x in the inner scope is {x}");
     }
     println!("The value of x is {x}");
+
+
+
+    fn add(x: f64, y: f64) -> f64 {
+        x+y
+    }
+
+    let result = add(2.2,3.3);
+
+    println!("the result of add(2,3) {result}"); 
+
+// remove semicolon for implicit return
+    // fn five() -> i32 {
+    //     5
+    // }
+
+    // println!("{:?}", five());
+
+
 }
